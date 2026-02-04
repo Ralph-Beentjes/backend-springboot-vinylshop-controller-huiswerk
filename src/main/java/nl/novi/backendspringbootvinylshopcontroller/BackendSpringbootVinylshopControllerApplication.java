d0171d09c736dae7653d0cdeee7026dab784cb5c
Ralph-Beentjes <ralphsimonbeentjes@gmail.com>
2026-02-04 15:53:27 +0100

    Opzet voor het huiswerk

---
+++ src/main/java/nl/novi/backendspringbootvinylshopcontroller/BackendSpringbootVinylshopControllerApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backendspringbootvinylshopcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringbootVinylshopControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringbootVinylshopControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/backendspringbootvinylshopcontroller/BackendSpringbootVinylshopControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backendspringbootvinylshopcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootVinylshopControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
